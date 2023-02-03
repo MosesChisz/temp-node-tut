121853db37780fdc9e8332602e1d7a5f247ee3bf
Moses <mosesc@cortexware.co.za>
2023-02-03 14:36:17 +0200

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,10 @@
+const names = require('./4-names')
+const sayHi = require('./5utils')
+const data = require('./6-alternative-favor')
+require('./7-mind-grinade')
+
+
+console.log(data)
+sayHi('kupakwashe')
+sayHi(names.john)
+sayHi(names.peter)
